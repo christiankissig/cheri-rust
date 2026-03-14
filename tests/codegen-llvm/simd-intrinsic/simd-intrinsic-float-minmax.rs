070b91dbae7f45cfaec89cc2133ef18f1fa98061
Edoardo Marangoni <ecmm@anche.no>
2026-03-14 17:35:18 +0100

    codegen-llvm: Add `ADDRSPACE` annotations after `call`.

--- tests/codegen-llvm/simd-intrinsic/simd-intrinsic-float-minmax.rs
+++ tests/codegen-llvm/simd-intrinsic/simd-intrinsic-float-minmax.rs
@@ -14,13 +14,13 @@ use core::intrinsics::simd::{simd_fmax, simd_fmin};
 // CHECK-LABEL: @fmin
 #[no_mangle]
 pub unsafe fn fmin(a: f32x4, b: f32x4) -> f32x4 {
-    // CHECK: call <4 x float> @llvm.minnum.v4f32
+    // CHECK: call[[ADDRSPACE]] <4 x float> @llvm.minnum.v4f32
     simd_fmin(a, b)
 }
 
 // CHECK-LABEL: @fmax
 #[no_mangle]
 pub unsafe fn fmax(a: f32x4, b: f32x4) -> f32x4 {
-    // CHECK: call <4 x float> @llvm.maxnum.v4f32
+    // CHECK: call[[ADDRSPACE]] <4 x float> @llvm.maxnum.v4f32
     simd_fmax(a, b)
 }
